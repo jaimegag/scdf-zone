e43b684a55bef270dc9eb1d484d157c5379cd638
jaimegag <jaguilar@pivotal.io>
2018-06-29 01:02:12 -0400

    Authentication for SCDF API calls

--- ci/register-maven-app.sh
+++ ci/register-maven-app.sh
@@ -8,9 +8,14 @@ THE_VERSION=$(grep -o '<version[^"]*' pom.xml | sed -e 's/<version>\(.*\)<\/vers
 
 echo "Registering proc1 ${THE_VERSION}"
 
+# Get token. client_id is cf to emulate cf CLI calls
+output=$(curl -X POST "https://login.sys.pcfgcp.jagapps.co/oauth/token" -d "grant_type=password&password=gonative&scope=&username=jaime&client_id=cf&client_secret=" -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: application/json" -k)
+token=$(jq -r '.access_token' <<< "$output")
+
 # Replace `<ROUTE>` with the route of SCDF-server running in CF.
 curl \
 	-X \
 	POST "http://dataflow-9c27ceda-b077-427f-b5fa-c07f606dc5a8.apps.pcfgcp.jagapps.co/apps/processor/proc1?force=true" \
 		-d "uri=https://s3.amazonaws.com/jaguilar-releases/processor-sample-${THE_VERSION}.jar" \
-	-v
+		-H "Authorization: bearer "${token} \
+	  -v
--- ci/update-maven-app.sh
+++ ci/update-maven-app.sh
@@ -8,10 +8,15 @@ THE_VERSION=$(grep -o '<version[^"]*' pom.xml | sed -e 's/<version>\(.*\)<\/vers
 
 echo "Updating demo stream in SCDF for PCF with ${THE_VERSION} of Maven artifact"
 
+# Get token. client_id is cf to emulate cf CLI calls
+output=$(curl -X POST "https://login.sys.pcfgcp.jagapps.co/oauth/token" -d "grant_type=password&password=gonative&scope=&username=jaime&client_id=cf&client_secret=" -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: application/json" -k)
+token=$(jq -r '.access_token' <<< "$output")
+
 # Replace `<ROUTE>` with the route of SCDF-server running in CF.
 curl \
 	-X \
 	POST "http://dataflow-9c27ceda-b077-427f-b5fa-c07f606dc5a8.apps.pcfgcp.jagapps.co/streams/deployments/update/demo" \
     -d '{"updateProperties":{"version.proc1":"'"${THE_VERSION}"'"},"releaseName":"proc1","packageIdentifier":{"packageName":"proc1"}}' \
     -H "Content-Type: application/json" \
+		-H "Authorization: bearer "${token} \
     -v
